44ecb1eaf154a7a56dabaea052bb38b1f6c3c61c
Walt Cobb <walt.cobb@ymail.com>
2017-07-24 18:58:46 -0500

    basic nmap

--- .l0wh4ngNmap.sh
+++ .l0wh4ngNmap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo $0
-
-nmap $1 -sV
+nmap $1 -sV > $1/scans/nmap.txt
 
 if (cat $1/scans/nmap.txt | grep 'try -Pn')
 	then
 		>&2 echo $1 ' is blocking our ping scans, using -Pn'
 		nmap $1 -Pn -sV
 fi
+
+cat $1/scans/nmap.txt | grep open > services.txt
