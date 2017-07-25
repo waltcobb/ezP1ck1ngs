15096d489c30117de35e6fbdafa5fc15a9a7dfd5
root <root@localhost.localdomain>
2017-07-25 07:49:28 -0400

    basic searchsploit added

--- .ezP1ck1ngsSearchSploit.sh
+++ .ezP1ck1ngsSearchSploit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 
-cat $1/services.txt | awk '{$1=$2=$3=""; print $0}' | awk 'NF' | cut -d ' ' -f 4- | grep "[A-Za-z]" | grep [0-9] > $1/.searchsploit.txt
+cat $1/services.txt | awk '{$1=$2=$3=""; print $0}' | awk 'NF' | cut -d '(' -f 1 | cut -d ' ' -f 4- | grep "[A-Za-z]" | grep [0-9] > $1/.searchsploit.txt
 
-while read line;do seachsploit "$line" ;done < $1/.seachsploit.txt > $1/possibleSplots.txt
+while read line;do searchsploit "$line" ;done < $1/.searchsploit.txt > $1/dirtyPossibleSploits.txt
 
-cat $1/possibleSplots.txt | grep \| | cut -d '|' -f 1 | sed -i '/Exploit Title/d' | sed -i '/^ *$/d'
+cat $1/dirtyPossibleSploits.txt | sed '/Exploit Title/d' | sed '/^ *$/d' | sed '/--------------/d' | sed '/^ /'d > $1/possibleSploits.txt 
+
+rm $1/dirtyPossibleSploits.txt
+
+#| grep \| | cut -d '|' -f 1 | sed '/Exploit Title/d' | sed '/^ *$/d' > $1/possibleSploits.txt
