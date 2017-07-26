b31a4b9a009dd01e9f58e666bc3afaa98b792b82
root <root@localhost.localdomain>
2017-07-26 18:20:27 -0400

    ddg support

--- .ezP1ck1ngsSearchSploit.sh
+++ .ezP1ck1ngsSearchSploit.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+mkdir $1/links
 
 cat $1/services.txt | awk '{$1=$2=$3=""; print $0}' | awk 'NF' | cut -d '(' -f 1 | cut -d ' ' -f 4- | grep "[A-Za-z]" | grep [0-9] > $1/.searchsploit.txt
 
+cat $1/services.txt | awk '{$1=$2=$3=""; print $0}' | awk 'NF'| cut -d '(' -f 1 | cut -d ' ' -f 4- | sed 's/ /+/g' > $1/.ddg.txt
+
 while read line;do searchsploit "$line" ;done < $1/.searchsploit.txt > $1/dirtyPossibleSploitsList.txt
+
+while read line;do ./.ddg.py "$line"+exploit-db | cut -d = -f 3 | grep exploit-db > $1/links/$line.txt ;done < $1/.ddg.txt
 
 cat $1/dirtyPossibleSploitsList.txt | sed '/Exploit Title/d' | sed '/^ *$/d' | sed '/--------------/d' | sed '/^ /'d > $1/possibleSploitsList.txt 
 
